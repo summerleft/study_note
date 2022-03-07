14be8c8076a7c1212882f283d31af5f8f83245e9
贺俊杰 <438058486@qq.com>
2022-03-07 23:55:03 +0800

    update: 初始化my-vue-router项目

---
+++ VUE/vue-router原理/my-vue-router/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/home">Home</router-link> ｜ 
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  routerLink {
+    display: block;
+  }
+</style>
