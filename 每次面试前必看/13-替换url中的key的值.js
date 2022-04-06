ea05d2c1dccd1389cfe72d20ac34f1749de69ef7
贺俊杰 <438058486@qq.com>
2022-04-06 23:22:32 +0800

    update

---
+++ 每次面试前必看/13-替换url中的key的值.js
@@ -0,0 +1,18 @@
+// 输入
+// url = 'https://www.nowcoder.com/discuss/v2/post?type=0&key=test'
+// key = 'abcd'
+// // 输出
+// res = 'https://www.nowcoder.com/discuss/v2/post?type=0&key=abcd'
+
+// function replaceKey(url, keyValue) {
+//     const urlObj = new URL(url);
+//     urlObj.searchParams.set('key', keyValue);
+//     return urlObj.href;
+// }
+
+function replaceKey(url, keyValue) {
+    
+     
+}
+
+console.log(replaceKey('https://www.nowcoder.com/discuss/v2/post?type=0&key=test', 'abcd'));
---
+++ 每次面试前必看/14-归并排序.js
@@ -0,0 +1,34 @@
+const mergeSort = arr => {
+    const len = arr.length;
+    if (len < 2) {
+        return arr;
+    }
+    let middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+    const result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length) {
+        result.push(left.shift());
+    }
+    while (right.length) {
+        result.push(right.shift());
+    }
+
+    return result;
+}
+
+console.log(mergeSort([3,6,1,7,3,2]))
